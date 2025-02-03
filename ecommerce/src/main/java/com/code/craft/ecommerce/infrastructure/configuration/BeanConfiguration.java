c1f78a77bc97f43d42dcfa140a0a6e2f007e83e0
Alex Breiner Delgado Hurtado <alexdehu@suramericana.com>
2025-02-03 12:16:25 -0500

    Created class of upload files and your bean configuracion.

--- ecommerce/src/main/java/com/code/craft/ecommerce/infrastructure/configuration/BeanConfiguration.java
+++ ecommerce/src/main/java/com/code/craft/ecommerce/infrastructure/configuration/BeanConfiguration.java
@@ -2,6 +2,7 @@ package com.code.craft.ecommerce.infrastructure.configuration;
 
 import com.code.craft.ecommerce.application.repository.ProductRepository;
 import com.code.craft.ecommerce.application.service.ProductService;
+import com.code.craft.ecommerce.application.service.UploadFile;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +12,10 @@ public class BeanConfiguration {
     @Bean
     public ProductService productService(ProductRepository productRepository) {
         return new ProductService(productRepository);
+    }
+
+    @Bean
+    public UploadFile uploadFile() {
+        return new UploadFile();
     }
 }
