af84765a6ecbd358a861633f97d5383e3c5dbb93
Alex Breiner Delgado Hurtado <alexdehu@suramericana.com>
2025-02-05 10:51:17 -0500

    add template for create exit and out for inventory with controller

--- ecommerce/src/main/java/com/code/craft/ecommerce/infrastructure/controller/StockController.java
+++ ecommerce/src/main/java/com/code/craft/ecommerce/infrastructure/controller/StockController.java
@@ -1,17 +1,40 @@
 package com.code.craft.ecommerce.infrastructure.controller;
 
+import com.code.craft.ecommerce.application.service.StockService;
+import com.code.craft.ecommerce.domain.Product;
+import com.code.craft.ecommerce.domain.Stock;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/admin/products/stock")
 public class StockController {
 
+    private final StockService stockService;
+
+    public StockController(StockService stockService) {
+        this.stockService = stockService;
+    }
+
     @GetMapping("/{id}")
     public String show(@PathVariable Integer id, Model model) {
+        Product product = new Product();
+        product.setId(id);
+        List<Stock> stocks = stockService.getStockByProduct(product);
+        model.addAttribute("stocks", stocks);
+        model.addAttribute("idProduct", product.getId());
         return "admin/stock/show";
     }
+
+    @GetMapping("/create-unit-product/{id}")
+    public String create(@PathVariable Integer id, Model model) {
+        model.addAttribute("idProduct", id);
+        return "admin/stock/create";
+    }
+
 }
