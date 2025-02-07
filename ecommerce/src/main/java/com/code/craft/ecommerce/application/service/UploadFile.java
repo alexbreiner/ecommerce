5fedcb332292becc6801fa1e1c8ebe736f46dcf3
Alex Breiner Delgado Hurtado <alexdehu@suramericana.com>
2025-02-07 11:07:06 -0500

    refactor load images

--- ecommerce/src/main/java/com/code/craft/ecommerce/application/service/UploadFile.java
+++ ecommerce/src/main/java/com/code/craft/ecommerce/application/service/UploadFile.java
@@ -10,8 +10,8 @@ import java.nio.file.Paths;
 
 public class UploadFile {
 
-    private final String FOLDER = "images//";
-    private final String IMG_DEFAULT = "default.jpg";
+    private static final String FOLDER = "images//";
+    private static final String IMG_DEFAULT = "default.jpg";
     private final File directory;
 
     public UploadFile() {
@@ -20,23 +20,20 @@ public class UploadFile {
 
     public String upload(MultipartFile multipartFile) throws IOException {
         if (!multipartFile.isEmpty()) {
-            String currentDir = System.getProperty("user.dir");
-            String folderPath = currentDir.concat(File.separator).concat(FOLDER);
-            createFolder(folderPath);
+            createFolder();
             try {
                 byte[] bytes = multipartFile.getBytes();
                 Path path = Paths.get(FOLDER + multipartFile.getOriginalFilename());
                 Files.write(path, bytes);
                 return multipartFile.getOriginalFilename();
             } catch (IOException e) {
-                e.printStackTrace();
                 throw new IOException("El archivo o directorio especificado no existe: " + e.getMessage());
             }
         }
         return IMG_DEFAULT;
     }
 
-    private void createFolder(String folderPath) {
+    private void createFolder() {
         if (!directory.exists()) {
             directory.mkdirs();
         }
